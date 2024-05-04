d5fabd1c5ef695d6f5c7206bf1e60e84d397e779
Lea Rosema <lea@lea.lgbt>
2024-05-04 22:16:59 +0200

    build: work on cmake mingw cross compile

--- scripts/mingw-build.sh
+++ scripts/mingw-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir -p build
 cd build
-cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=../cmake/x86_64-w64-mingw32.cmake .. 
+
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/x86_64-w64-mingw32.cmake ..
 make
 
