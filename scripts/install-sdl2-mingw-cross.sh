cec2932a6a484d740d59ebd03043646d3af4928b
Lea Rosema <lea@lea.lgbt>
2024-05-04 22:47:53 +0200

    build: add make cross commands

--- scripts/install-sdl2-mingw-cross.sh
+++ scripts/install-sdl2-mingw-cross.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 SDL_VERSION=2.30.2
 SDL_IMAGE_VERSION=2.8.2
-DEST=vendor/mingw
+
+cd $(dirname $0)
+cd ..
+ROOT_DIR=$(pwd)
+DEST=${ROOT_DIR}/vendor/mingw
 
 mkdir -p ${DEST}
 echo "Prebuilt libraries for cross compilation to mingw" > ${DEST}/README.md
 curl -s -L https://github.com/libsdl-org/SDL/releases/download/release-${SDL_VERSION}/SDL2-devel-${SDL_VERSION}-mingw.tar.gz | tar xvzf - -C ${DEST}
 curl -s -L https://github.com/libsdl-org/SDL_image/releases/download/release-${SDL_IMAGE_VERSION}/SDL2_image-devel-${SDL_IMAGE_VERSION}-mingw.tar.gz | tar xvzf - -C ${DEST}
 
-# mkdir -p /usr/local/i686-w64-mingw32 /usr/local/x86_64-w64-mingw32
-# cd mingw/SDL2-${SDL_VERSION}
-# make cross
+mkdir -p /usr/local/i686-w64-mingw32 /usr/local/x86_64-w64-mingw32
+if test -d /usr/local/x86_64-w64-mingw32/bin/SDL2.dll; then
+	echo "Found a mingw32 SDL2.dll installed, skipping make cross"
+else
+	cd ${DEST}/SDL2-${SDL_VERSION}
+	make cross
+	cd ${ROOT_DIR}
+fi
 
-# cd ../SDL2_image-${SDL_IMAGE_VERSION}
-# make cross
+if test -d /usr/local/x86_64-w64-mingw32/bin/SDL2_image.dll; then
+	echo "Found a mingw32 SDL2_image.dll installed, skipping make cross"
+else
+	cd ${DEST}/SDL2_image-${SDL_IMAGE_VERSION}
+	make cross
+	cd ${ROOT_DIR}
+fi
