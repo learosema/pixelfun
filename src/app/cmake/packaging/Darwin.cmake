9fc3a06e428ab00cf422fafb61aaf1dc1b267f3e
Lea Rosema <lea@lea.lgbt>
2024-05-23 13:14:46 +0200

    fix: fix hard-coded app name and setting in Info.plist

--- src/app/cmake/packaging/Darwin.cmake
+++ src/app/cmake/packaging/Darwin.cmake
@@ -10,16 +10,17 @@ endif ()
 add_custom_command(TARGET ${NAME} POST_BUILD
   COMMAND ${CMAKE_COMMAND} -E copy_if_different
   $<TARGET_FILE:SDL2::SDL2>
-  $<TARGET_FILE_DIR:App>/../Frameworks/$<TARGET_FILE_NAME:SDL2::SDL2>)
+  $<TARGET_FILE_DIR:${NAME}>/../Frameworks/$<TARGET_FILE_NAME:SDL2::SDL2>)
 
 # For distribution without XCode:
 if (NOT "${CMAKE_GENERATOR}" STREQUAL "Xcode")
-  install(FILES $<TARGET_FILE:SDL2::SDL2> DESTINATION $<TARGET_FILE_DIR:App>/../Frameworks/)
+  install(FILES $<TARGET_FILE:SDL2::SDL2> DESTINATION $<TARGET_FILE_DIR:${NAME}>/../Frameworks/)
 endif ()
 
 # macOS package settings
 string(TIMESTAMP CURR_YEAR "%Y")
 set_target_properties(${NAME} PROPERTIES
+  EXECUTABLE_NAME "${NAME}"
   XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY ""
   MACOSX_BUNDLE_BUNDLE_VERSION "${BUILD_VERSION}"
   MACOSX_BUNDLE_SHORT_VERSION_STRING "${PROJECT_VERSION}"
